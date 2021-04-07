b90b28f06bc144df4124cea0f7c74c058255a7a0
JMCARTER12 <82064669+JMCARTER12@users.noreply.github.com>
2021-04-07 16:01:40 -0500

    Add files via upload

---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,47 @@
+﻿// Grade ID: R8206
+// Due date: 3/29/2020
+// Lab 7
+// Section 1
+// This program writes out * in a square shape given the users input of rows desired
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring variables
+            int input;
+            string numStars;
+            Console.WriteLine("Stars Per Side Desired?: "); // converting user input using TryParse method
+            numStars = Console.ReadLine();
+            int.TryParse(numStars, out input);
+            ShowSquareOfStars(input); // Gives the output of stars
+
+        }
+        public static void ShowSquareOfStars(int input) // the void method starts here
+        {
+            
+            int MIN_ROWS = 0;
+            if (input > MIN_ROWS)
+                for (int row = 0; row < input; ++row) // for statements make the square with rows and columns
+                {
+                    for (int columns = 0; columns < input; ++columns)
+                        Console.Write("* ");
+                    Console.WriteLine();
+                }
+            else
+            {
+                Console.WriteLine("Enter Valid Input:");
+                Console.ReadLine();
+            }
+                Console.WriteLine("");
+        }
+    }
+}
